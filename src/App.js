3eab7d569ff510a60c63b619107469b3bc1c86f2
Joshua Martin <the.manuscriptmaster@gmail.com>
2018-12-17 16:59:33 -0500

    Basic observable in React app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import { fromEvent } from 'rxjs';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.observable = null;
+    this.state = {
+      count: 0
+    };
+  }
+  componentDidMount() {
+    this.observable = fromEvent(window, 'click')
+      .subscribe(() => this.setState({ count: this.state.count + 1 }))
+  }
+
+  componentWillUnmount() {
+    this.observable.complete();
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello RxJS</h1>
+        <p>The current count is {this.state.count}</p>
       </div>
     );
   }
